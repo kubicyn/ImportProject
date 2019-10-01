ee2fb708297f4b0fa5e5927f2747b2c9e951b2b0
kubicyn <kubicyn@gmail.com>
2019-10-01 22:05:49 +0200

    co zrobic

---
+++ main.py
@@ -0,0 +1,13 @@
+#s3.zylowski.net/public/input/6.txt
+
+"""
+[Student A] Menu of application
+[Student B] Downloading text file from the internet
+[Student A] Count letters
+[Student B] Count words
+[Student C] Count punctuation marks
+[Student C] Count sentences
+[Student A] Generate report about count of every letter in the file
+[Student C] Save statistics to the file
+[Student B] Close the application
+"""
