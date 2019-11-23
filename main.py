5e460da9ea9c0812780904dbd302953f08de15f7
vampsas <sandra.szablewska@onet.pl>
2019-11-23 20:19:09 +0100

    Close #20

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def pun_marks_count():
     read_file = file_to_load.read()
     my_list = []
     for c in read_file:
-        if c in string.punctuation:
+        if c == "?" or ".":
             my_list.append(c)
     number_of_marks = len(my_list)
     raport = open("raport.txt", "a+")
